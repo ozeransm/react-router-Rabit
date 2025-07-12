663de75541d7b50e8720a4bb80931448159281d5
Serhii <ozeransm@gmail.com>
2025-07-12 22:19:02 +0200

    update page Admin, styled fields

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -1,10 +1,31 @@
 import { useForm } from 'react-hook-form';
 import type { SubmitHandler } from 'react-hook-form';
+import styled from 'styled-components';
 type Inputs = {
   name: string;
   price: string;
   description: string;
 };
+const StyledForm = styled.form`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;
+  align-items: center;
+`;
+const StyledFormDiv = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+  align-items: center;
+`;
+const StyledFormField = styled.input`
+`;
+const StyledFormTextArea = styled.textarea`
+`;
+const StyledButton = styled.input`
+display: block;
+`;
 
 export default function Admin() {
   const {
@@ -23,20 +44,22 @@ export default function Admin() {
     <div>
       <h2>Administrator</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
         {/* register your input into the hook by invoking the "register" function */}
-        <input {...register('name', { required: true })} placeholder="Name" />
+        <StyledFormField {...register('name', { required: true })} placeholder="Name" />
         {errors.name && <span>This field name is required</span>}
         {/* include validation with required or other standard HTML validation rules */}
-        <input {...register('price', { required: true })} placeholder="Price" />
+        <StyledFormField {...register('price', { required: true })} placeholder="Price" />
         {/* errors will return when field validation fails  */}
         {errors.price && <span>This field price is required</span>}
-        <textarea
+        </div>
+        <StyledFormTextArea
           {...register('description', { required: true })}
           placeholder="Description"
           rows={5}
         />
         {errors.description && <span>This field description is required</span>}
-        <input type="submit" />
+        <StyledButton type="submit" />
       </form>
     </div>
   );
