b278a60354839ff3465957829703f94fd3c87a3b
Serhii <ozeransm@gmail.com>
2025-07-12 21:00:42 +0200

    create page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Admin from './Pages/Admin';
+import Home from './Pages/Home';
+import Orders from './Pages/Orders';
+import Contacts from './Pages/Contacts';
+import NoMatch from './Pages/NoMatch';
+import Layout from './Pages/Layout';
 type AppProps = {
   products: string;
 };
@@ -35,9 +41,9 @@ export default function App({ products }: AppProps) {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home products={products} />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
+          <Route path="admin" element={<Admin />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="contacts" element={<Contacts />} />
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
@@ -48,72 +54,3 @@ export default function App({ products }: AppProps) {
   );
 }
 
-function Layout() {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </div>
-  );
-}
-
-function Home({ products }: AppProps) {
-  console.log('Тип:', typeof products);
-  console.log('name:', products);
-  return (
-    <div>
-      <h2>Home</h2>
-      <div>Name: {products}</div>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+type AppProps = {
+  products: string;
+};
+export default function Home({ products }: AppProps){
+    return(
+        <div>
+            <h2>Home</h2>
+            <div>Name: {products}</div>
+        </div>
+    )
+}
