dc019f62cfd38df179438d1ce5140101ce312d1f
Serhii <ozeransm@gmail.com>
2025-07-15 23:38:04 +0200

    npm run format

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -5,7 +5,7 @@ import 'swiper/css';
 import 'swiper/css/grid';
 import { Grid } from 'swiper/modules';
 const StyledBaseField = styled.div`
- border: 1px solid lightblue;
+  border: 1px solid lightblue;
   border-radius: 10px;
   padding: 40px;
 `;
@@ -25,45 +25,53 @@ const StyledImg = styled.img`
 
   @media (min-width: 1024px) {
     width: 420px;
-  } 
+  }
 `;
 export default function Card({ products, rows }: AppProp) {
-  
   let row = 1;
   let slidePreView = 1;
   let direction: 'horizontal' | 'vertical' = 'horizontal';
-  switch(rows){
-    case 3: slidePreView = 1;row=3;direction = 'vertical';
-    break;
-    case 2: slidePreView = 1;row=2;direction='horizontal';
-    break;
-    case 1: slidePreView = 3;row=1;direction='horizontal';
-    break;
-    default: slidePreView = 1;direction='horizontal';
+  switch (rows) {
+    case 3:
+      slidePreView = 1;
+      row = 3;
+      direction = 'vertical';
+      break;
+    case 2:
+      slidePreView = 1;
+      row = 2;
+      direction = 'horizontal';
+      break;
+    case 1:
+      slidePreView = 3;
+      row = 1;
+      direction = 'horizontal';
+      break;
+    default:
+      slidePreView = 1;
+      direction = 'horizontal';
   }
-  console.log(row, slidePreView)
+  console.log(row, slidePreView);
   return (
     <StyledBaseField>
-    <Swiper
-    
-      // direction={direction}
-      modules={[Grid]}
-      spaceBetween={10}
-      slidesPerView={slidePreView}
-      grid={{
-        rows: row,
-        fill: 'row',
-      }}
-      
-    >
-      {products.map((el) => (
-        <SwiperSlide key={el.id}>
-          <p>{el.name}</p>
-          <p>{el.price}</p>
-          <StyledImg src={el.img} alt={el.name} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <Swiper
+        // direction={direction}
+        modules={[Grid]}
+        spaceBetween={10}
+        slidesPerView={slidePreView}
+        grid={{
+          rows: row,
+          fill: 'row',
+        }}
+      >
+        {products.map((el) => (
+          <SwiperSlide key={el.id}>
+            <p>{el.name}</p>
+            <p>{el.price}</p>
+            <StyledImg src={el.img} alt={el.name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </StyledBaseField>
   );
 }
