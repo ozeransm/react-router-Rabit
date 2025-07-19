33643583c86484cbb09027f08da309202a0b6554
Serhii <ozeransm@gmail.com>
2025-07-19 16:50:26 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,42 @@
+// import * as path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import rollupReplace from '@rollup/plugin-replace';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     sourcemap: false,
+//   },
+//   server: {
+//     port: 3000,
+//     sourcemap: false,
+//     historyApiFallback: true,
+//   },
+//   plugins: [
+//     rollupReplace({
+//       preventAssignment: true,
+//       values: {
+//         'process.env.NODE_ENV': JSON.stringify('development'),
+//       },
+//     }),
+//     react(),
+//   ],
+//   resolve: process.env.USE_SOURCE
+//     ? {
+//         alias: {
+//           'react-router': path.resolve(
+//             __dirname,
+//             '../../packages/react-router/index.ts'
+//           ),
+//           'react-router-dom': path.resolve(
+//             __dirname,
+//             '../../packages/react-router-dom/index.tsx'
+//           ),
+//         },
+//       }
+//     : {},
+// });
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -20,7 +59,11 @@ export default defineConfig({
         'process.env.NODE_ENV': JSON.stringify('development'),
       },
     }),
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-styled-components'],
+      },
+    }),
   ],
   resolve: process.env.USE_SOURCE
     ? {
